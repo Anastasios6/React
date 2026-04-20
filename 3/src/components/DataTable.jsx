36a7d39cf5e026a7e3921b9a08c1467d3b28588a
Anastasios <hadzijanev6@gmail.com>
2026-04-20 13:47:18 +0200

    added switch for table visibility

--- 3/src/components/DataTable.jsx
+++ 3/src/components/DataTable.jsx
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const DataTable = ({ users }) => {
+  const [tableVisibilty, setTableVisibility] = useState(false);
+  const tableToggle = () => {
+    setShowTable(!table);
+  };
   return (
     <div id="tableContainer">
-      <table border={1} width={100}>
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>Password</th>
-            <th>E-mail</th>
-            <th>Contact number</th>
-            <th>Comment</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, i) => (
-            <tr key={i}>
-              <td>{user.username}</td>
-              <td>{user.password}</td>
-              <td>{user.email}</td>
-              <td>{user.tel}</td>
-              <td>{user.comment}</td>
+      <button
+        className="tableVisibility"
+        onClick={() => {
+          setTableVisibility(!tableVisibilty);
+        }}
+      >
+        {tableVisibilty ? "Hide users table" : "Show Users table"}
+      </button>
+      {tableVisibilty && (
+        <table border={1} width={100}>
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Password</th>
+              <th>E-mail</th>
+              <th>Contact number</th>
+              <th>Comment</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((user, i) => (
+              <tr key={i}>
+                <td>{user.username}</td>
+                <td>{user.password}</td>
+                <td>{user.email}</td>
+                <td>{user.tel}</td>
+                <td>{user.comment}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
